8a7dc3b32756e2280a0d6cc95e959223b210decc
miguelbuelna <miguel.buelna@potros.itson.edu.mx>
2025-04-28 00:19:15 -0700

    lalalaa

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,9 +4,9 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:5173', 
+        env('FRONTEND_URL', 'http://localhost:5173'),
         'https://greeklosophy-app-good.vercel.app',
-    ],
+],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
