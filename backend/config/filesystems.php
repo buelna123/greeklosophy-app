169342c60208f0bf48dbe343fbe66698d7917d92
miguelbuelna <miguel.buelna@potros.itson.edu.mx>
2025-04-30 18:38:33 -0700

    integracion de cloudinary PARECE BIEN 9

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -22,9 +22,14 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'cloud' => [
+                'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                'api_key'    => env('CLOUDINARY_API_KEY'),
+                'api_secret' => env('CLOUDINARY_API_SECRET'),
+            ],
+            'url' => [
+                'secure' => true,
+            ],
         ],
 
         's3' => [
